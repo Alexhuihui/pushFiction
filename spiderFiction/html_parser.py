0fe807de79f47a072c0fa399bba2c0689c80baf0
wangyonghui <2930807240@qq.com>
2018-12-28 15:21:44 +0800

    获取章节列表

--- spiderFiction/html_parser.py
+++ spiderFiction/html_parser.py
@@ -1,21 +1,27 @@
-from bs4 import BeautifulSoup
 import re
-import urllib.parse
+
+from bs4 import BeautifulSoup
+
 
 class HtmlParser(object):
-    def get_new_data(self, page_url, soup):
-        res_data = []
-        anodes = soup.find_all('p')
-        for anode in anodes:
-            res_data.append(anode.get_text())
-        
-        return res_data
-
-    def parse(self, page_url, html_cont):
-        if page_url is None or html_cont is None:
-            return
-
-        soup = BeautifulSoup(html_cont, 'html.parser')
-        new_data = self.get_new_data(page_url, soup)
-
-        return new_data
+	def get_new_data(self, soup):
+		res_data = []
+		anodes = soup.find_all('p')
+		for anode in anodes:
+			res_data.append(anode.get_text())
+		
+		return res_data
+	
+	def parse(self, page_url, html_cont):
+		if html_cont is None:
+			return
+		
+		soup = BeautifulSoup(html_cont, features="html.parser")
+		novel_id = page_url[-6:-1]
+		
+		for link in soup.find_all('a', href=re.compile("\d\.html")):
+			chapter_title = link.get_text()
+			chapter_url = page_url + link.get('href')[12:]
+			chapter_id = link.get('href')[12:-5]
+			
+			
--- spiderFiction/spider_main.py
+++ spiderFiction/spider_main.py
@@ -8,14 +8,20 @@ class SpiderMain(object):
 		self.parser = html_parser.HtmlParser()
 		self.outputer = html_output.HtmlOutputer()
 		self.dao = dao.Dao()
-		
+	
 	# 返回所有的小说的地址
 	def get_urls(self):
 		novel_urls = self.dao.select_novel()
 		return novel_urls
 	
-	# 根据小说地址爬取小说的章节
-	def craw_chapter(self, urls):
+	# 根据单个的小说地址获取该本书所有的章节地址
+	def get_chapter(self, url):
+		print(url)
+		html_content = self.downloader.download(url)
+		self.parser.parse(url, html_content)
+	
+	# 根据小说的章节地址爬取内容
+	def craw_content(self, urls):
 		count = 1
 		self.urls.add_new_urls(urls)
 		while self.urls.has_new_url():
@@ -37,15 +43,18 @@ class SpiderMain(object):
 				
 				count += 1
 			
-			except:
-				print('craw failed')
+			except Exception as e:
+				print(e + 'craw failed')
 		
 		self.outputer.output_html()
 	
-	def craw_content(self, url):
-		pass
+	# 控制函数
+	def main(self):
+		urls = self.get_urls()
+		for url in urls:
+			self.get_chapter(url)
 
 
 if __name__ == '__main__':
 	obj_spider = SpiderMain()
-	obj_spider.get_urls()
+	obj_spider.main()
